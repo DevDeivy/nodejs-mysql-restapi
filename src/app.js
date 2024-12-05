53ca57607c9a928c9521752bd4eec0b2030f4952
deivy Lujan <deivylujans@gmail.com>
2024-12-04 23:31:09 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@
 import express from 'express'
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
-import {PORT} from './config.js'
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(indexRoutes);
 app.use('/api', employeesRoutes);
